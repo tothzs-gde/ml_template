0b4060ca78d0e22231ac395114a58d8ef584791c
Zsombor Toth <227781169+tothzs-gde@users.noreply.github.com>
2025-09-17 13:40:25 +0200

    Update

--- src/model/inference.py
+++ src/model/inference.py
@@ -56,8 +56,8 @@ def infer(X_data: list[dict[str, Any]], model_name: str, model_version: str):
         drifted = False
         for col, results in drift_results.items():
             drifted = drifted and results["drifted"]
-            mlflow.log_metric(f"drift_score_{col}", results["drift_score"])
-            mlflow.log_metric(f"drift_bool_{col}", results["drifted"])
+            mlflow.log_metric(f"drift_{col}", results["drifted"])
+            mlflow.log_metric(f"drift_{col}_score", results["drift_score"])
         
         mlflow.set_tag(f"data_drifted", str(drifted))
 
